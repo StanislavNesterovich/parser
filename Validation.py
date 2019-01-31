7dcff449b577a175833470fa52b6fb1e7b005faf
Stanislav Nesterovich <stanislau.nestsiarovich@netcracker.com>
2019-01-31 19:49:34 +0300

    switch validator

--- Validation.py
+++ Validation.py
@@ -1,25 +1,6 @@
-from cerberus import Validator
-import yaml
-
-with open('schema.yaml', 'r') as f:
-    style = yaml.load(f)
-
-def oddity(field, value, error):
-    if not value & 1:
-        error(field, "Must be an odd number")
+from pykwalify.core import Core
 
 
-def load_doc():
-    with open("document.yaml", 'r') as stream:
-        try:
-            return yaml.load(stream)
-        except yaml.YAMLError as exception:
-            raise exception
+c = Core(source_file="document.yaml", schema_files=["schema.yaml"])
+c.validate(raise_exception=True)
 
-schema = eval(open('schema', 'r').read())
-
-valid = Validator(style)
-doc = load_doc()
-
-print valid.validate(doc, style)
-print valid.errors
